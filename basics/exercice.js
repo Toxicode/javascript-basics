00a41be701906b5ea66782a7c9c3463ec79b7ece
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-05-06 22:07:01 +0200

    Add Oracle exercice

---
+++ basics/exercice.js
@@ -0,0 +1,25 @@
+// Créer un oracle qui répond Oui ou Non en fonction de la taille de la question
+
+/*
+function startQuestionsGame() {
+    var question = prompt('Posez votre question ?');
+    var reponse;
+    var suite = true;
+    
+    if (question === 'stop') {
+        suite = false;
+        reponse = 'merci d\'avoir joué'
+    } else if (question.length >= 15) {
+        reponse = 'Oui';
+    } else {
+        reponse = 'Non';
+    }
+
+    toxicode.console.log(reponse);
+    if (suite) {
+        startQuestionsGame();
+    }
+}
+
+startQuestionsGame();
+*/
